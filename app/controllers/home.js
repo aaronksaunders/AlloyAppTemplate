4619c92074d24e7d529523ca15a8006eb74a4c9b
Aaron Saunders <aaron@clearlyinnovative.com>
2015-02-01 19:01:44 -0500

    initial commit of the table view sample using the starter app from ricardo

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,37 +1,77 @@
+//controllers/home.js
+
 /*
-Even though your Views are platform-specific, you can still keep your controllers cross-platform.
-This controller will be used for your home View regardless of the platform, so you'll have to
-manually check in case you need to perform platform-specific operations
-*/
+ Even though your Views are platform-specific, you can still keep your controllers cross-platform.
+ This controller will be used for your home View regardless of the platform, so you'll have to
+ manually check in case you need to perform platform-specific operations
+ */
 
 var args = arguments[0] || {};
 
-if (!Alloy.isTablet){
-  if (OS_IOS){
-    $.sharedhome.greeting.text = "Hello from iOS";
-  }else if (OS_ANDROID){
-    $.sharedhome.greeting.text = "Hello from Android"
-  }
-}else{
-  if (OS_IOS){
-    console.log('ipad');
-    $.sharedhome.greeting.text = "Hello from iPad";
-  }else if (OS_ANDROID){
-    $.sharedhome.greeting.text = "Hello from Android Tablet"
-  }
-}
+//defining the collection in the controller rather than the view
+Alloy.Collections.instance("cars");
 
-function doopen(evt){
-	if (OS_ANDROID){
-		var abx=require('com.alcoapps.actionbarextras');
+//add the data to the collection after the  view-controller pair is created
+Alloy.Collections.cars.reset([{
+	"make" : "Honda",
+	"model" : "Civic"
+}, {
+	"make" : "Honda",
+	"model" : "Accord"
+}, {
+	"make" : "Ford",
+	"model" : "Escape"
+}, {
+	"make" : "Ford",
+	"model" : "Mustang"
+}, {
+	"make" : "Nissan",
+	"model" : "Altima"
+}]);
+
+// we need to let the sharedhome controller know about the parent
+// Navigation window so it can properly open windows on IOS
+OS_IOS && $.sharedhome.setNavWindow($.nav);
+
+function doopen(evt) {
+	if (OS_ANDROID) {
+		var abx = require('com.alcoapps.actionbarextras');
 		abx.setBackgroundColor('#dddddd');
 	}
 }
 
-function doadd(evt){
-	if (OS_IOS){
-		$.nav.openWindow(Alloy.createController('secondwin').getView());
-	}else if (OS_ANDROID){
-		Alloy.createController('secondwin').getView().open();
+function doadd(evt) {
+	if (OS_IOS) {
+		openAsModal(Alloy.createController('createItem').getView(), $.nav);
+	} else if (OS_ANDROID) {
+		Alloy.createController('createItem').getView().open();
+	}
+}
+
+/**
+ * helper function for opening a modal window in IOS with the
+ * proper title bar
+ *
+ * @param {Object} _view
+ */
+function openAsModal(_view, _navView) {
+	if (OS_IOS) {
+
+		var navWindow = _navView;
+
+		if (!_navView) {
+			navWindow = Titanium.UI.iOS.createNavigationWindow({
+				window : _view
+			});
+		}
+
+		_view.navWindow = navWindow;
+		navWindow.open({
+			modal : true
+		});
+	} else {
+		_view.open({
+			modal : true
+		});
 	}
 }
--- app/controllers/sharedhome.js
+++ app/controllers/sharedhome.js
@@ -1,1 +1,87 @@
 var args = arguments[0] || {};
+var navWindow = null;
+
+/**
+ * we need this to open the windows using the navigation window
+ * object on IOS. Since this is a view being added as required
+ * 
+ * @see- http://docs.appcelerator.com/titanium/3.0/#!/guide/Alloy_XML_Markup-section-35621528_AlloyXMLMarkup-RequireElement
+ */
+$.setNavWindow = function(_navWindow) {
+	navWindow = _navWindow;
+};
+
+
+//controllers/cars.js
+function transform(model) {
+	//convert the model to a JSON object
+	var carObject = model.toJSON();
+	var output = {
+		"title" : carObject.model + " by " + carObject.make,
+		"id" : model.cid
+	};
+
+	console.log(output);
+	return output;
+}
+
+//show only cars made by Honda
+function filter(collection) {
+	return collection.where({
+		make : "Honda"
+	});
+}
+
+//this is an event listener to ensure that the TalbleView bindings are cleaned up
+//correctly and no memory leaks are left
+
+//Free the model-view data binding resources when the view-controller closes
+$.getView().addEventListener("close", function() {
+	$.destroy();
+});
+
+$.table.addEventListener('click', function(_event) {
+
+	//get the correct approach
+	//
+	// The properties synch adapter that is provided by appcelerator does not set the model.id so get
+	// will never work. See the appcelerator documentation on Backbone Sync Adapters
+	var model = Alloy.Collections.cars.getByCid(_event.rowData.modelId);
+	//var model = Alloy.Collections.cars.get(_event.rowData.modelId);
+
+	//create the controller and pass the model to it
+	var detailWindowController = Alloy.createController("detailWindow", {
+		data : model
+	});
+
+	//get view returns to root view when no view ID is provided
+	if (OS_IOS) {
+		navWindow.openWindow(detailWindowController.getView());
+	} else if (OS_ANDROID) {
+		detailWindowController.getView().open();
+	}
+
+});
+
+/**
+ * helper function for opening a modal window in IOS with the
+ * proper title bar
+ *
+ * @param {Object} _view
+ */
+function openAsModal(_view) {
+	if (OS_IOS) {
+		var navWindow = Titanium.UI.iOS.createNavigationWindow({
+			window : _view
+		});
+
+		_view.navWindow = navWindow;
+		navWindow.open({
+			modal : true
+		});
+	} else {
+		_view.open({
+			modal : true
+		});
+	}
+}
